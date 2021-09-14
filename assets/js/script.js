264583af907882c51c3ec3f40f8e1c71968a0ae1
Vielay B <vielayb@gmail.com>
2021-09-14 17:05:41 -0400

    YouTube Player

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,24 @@ fetch(
     .then(function(data) {
         console.log(data);
     });
+
+   
+//     fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+// 		"x-rapidapi-key": "70461e1ed8mshe24dfc99ab92ee2p14e7e2jsn62d271d2b794"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+
+
+// information about this movie
+//budget
+//title
+//overview
