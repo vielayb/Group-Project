6c32707fb9e1a26ad72a6bc031601863e90c3620
Anthony Her <antpher@gmail.com>
2021-09-14 19:23:11 -0400

    search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-fetch(
-    "https://api.themoviedb.org/3/movie/550?api_key=fcce2b5c2f58b9fb122efde3fc3fca79"
-)
+var inputValue = document.querySelector('.inputValue')
+var key = 'fcce2b5c2f58b9fb122efde3fc3fca79'
+var movieName = document.querySelector('.movieName')
+var rating = document.querySelector('.rating')
+var overview = document.querySelector('.overview')
+var release = document.querySelector('.release')
+
+function getMovie(event) {
+  event.preventDefault();
+  var search = inputValue.value
+  fetch('https://api.themoviedb.org/3/search/movie?api_key=' + key + '&query=' + search + '')
+    
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
     });
+    var name = [0].title
+    movieName.innerHTML = name
+    var ratingScore = [0].vote_average
+    rating.innerHTML = ratingScore
+    var overviewEl = [0].overview
+    overview.innerHTML = overviewEl
+    var releaseDateEl = [0].release_date
+    release.innerHTML = releaseDateEl
+  
+  }
 
-   
-
+document.querySelector('.submitBtn').addEventListener('submit', getMovie)
