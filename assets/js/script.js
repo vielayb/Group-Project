2e153ff37ffbe0d0e44d67bf84bba192007aca1f
Vielay B <vielayb@gmail.com>
2021-09-14 20:55:06 -0400

    Trailer Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var movieName = document.querySelector('.movieName')
 var rating = document.querySelector('.rating')
 var overview = document.querySelector('.overview')
 var release = document.querySelector('.release')
-
+var iframe = document.getElementById("trailer");
 function getMovie(event) {
   event.preventDefault();
   var search = inputValue.value
@@ -24,6 +24,25 @@ function getMovie(event) {
         overview.innerHTML = overviewEl
         var releaseDateEl = data.results[0].release_date
         release.innerHTML = releaseDateEl
+        // my code
+                //path to movie id
+                var movieId = data.results[0].id;
+                console.log(movieId);
+                //results for more information including path to YouTube Trailer
+                return fetch(
+                    "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=fcce2b5c2f58b9fb122efde3fc3fca79&append_to_response=videos"
+                )
+                        .then(function(response) {
+                            return response.json();
+                        })
+                        .then(function(infoObject) {
+                            console.log(infoObject);
+
+                            var trailerPath = infoObject.videos.results[0].key;
+                            console.log(trailerPath);
+
+                            iframe.setAttribute('src', 'https://www.youtube.com/embed/' + trailerPath +'');
+                        });
     });
 
   
